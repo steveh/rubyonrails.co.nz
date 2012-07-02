6a0887883073861e81eae0b36500a8085eb06f09
Steve Hoeksema <steve@seven.net.nz>
2012-07-03 11:34:21 +1200

    Add localist

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,4 @@
 ROR_GITHUB_USER = 'rornz'
 ROR_CALENDAR = 'http://www.google.com/calendar/feeds/2si764v3mdv3b7vm3vie3s0hdg%40group.calendar.google.com/public/basic'
-ROR_NZ_ORGANISATIONS = ['resolve', 'trineo'] # organisations can't follow this account so add yours here
+ROR_NZ_ORGANISATIONS = ['resolve', 'trineo', 'localist'] # organisations can't follow this account so add yours here
 ROR_REQUIRED_WATCHERS = 5 # only show projects with more than this many watchers
